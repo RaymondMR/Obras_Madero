9a42b4031579dce29e38a94e3f3df2a301919ba8
RaymondMR <ramonmontoya@outlook.com>
2023-03-20 12:25:25 -0600

    Se agregaron algunos Archivos 01.txt

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
  path('', views.HomeView.as_view(), name='home'),
  path('authorized/', views.AuthorizedView.as_view()),
+ path('registrarse', views.SignupView.as_view(), name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
 
 class HomeView(TemplateView):
     template_name="home/welcome.html"
@@ -10,6 +14,17 @@ class HomeView(TemplateView):
 class AuthorizedView(LoginRequiredMixin,TemplateView):
     template_name="home/authorized.html"
     login_url='/admin'
+
+class SignupView(CreateView):
+    form_class=UserCreationForm
+    template_name='home/register.html'
+    success_url='/obras'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super().get(request, *args, **kwargs)
+	
 
 
 
